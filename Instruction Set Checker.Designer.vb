f8261bd124d07f84a869908c99518047f191e65d
Jing Xian <jingxian_98@hotmail.com>
2021-02-17 15:54:59 +0800

    Add project files.

---
+++ Instruction Set Checker.Designer.vb
@@ -0,0 +1,79 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class InstructionSetChecker
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(InstructionSetChecker))
+        Me.ButtonCheck = New System.Windows.Forms.Button()
+        Me.LabelTitle = New System.Windows.Forms.Label()
+        Me.LabelCopyright = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'ButtonCheck
+        '
+        Me.ButtonCheck.Location = New System.Drawing.Point(89, 45)
+        Me.ButtonCheck.Name = "ButtonCheck"
+        Me.ButtonCheck.Size = New System.Drawing.Size(162, 51)
+        Me.ButtonCheck.TabIndex = 0
+        Me.ButtonCheck.Text = "Check Compatibility"
+        Me.ButtonCheck.UseVisualStyleBackColor = True
+        '
+        'LabelTitle
+        '
+        Me.LabelTitle.AutoSize = True
+        Me.LabelTitle.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LabelTitle.Location = New System.Drawing.Point(-1, 7)
+        Me.LabelTitle.Name = "LabelTitle"
+        Me.LabelTitle.Size = New System.Drawing.Size(346, 25)
+        Me.LabelTitle.TabIndex = 1
+        Me.LabelTitle.Text = "SSE/AVX Compatibility Checker"
+        '
+        'LabelCopyright
+        '
+        Me.LabelCopyright.AutoSize = True
+        Me.LabelCopyright.Location = New System.Drawing.Point(59, 101)
+        Me.LabelCopyright.Name = "LabelCopyright"
+        Me.LabelCopyright.Size = New System.Drawing.Size(227, 13)
+        Me.LabelCopyright.TabIndex = 2
+        Me.LabelCopyright.Text = "UHS PREMIUM © 2021 | All Rights Reserved "
+        '
+        'InstructionSetChecker
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(344, 114)
+        Me.Controls.Add(Me.LabelCopyright)
+        Me.Controls.Add(Me.LabelTitle)
+        Me.Controls.Add(Me.ButtonCheck)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "InstructionSetChecker"
+        Me.Text = "Instruction Set Checker"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents ButtonCheck As Button
+    Friend WithEvents LabelTitle As Label
+    Friend WithEvents LabelCopyright As Label
+End Class
---
+++ Instruction Set Checker.vb
@@ -0,0 +1,22 @@
+﻿Public Class InstructionSetChecker
+    <Runtime.InteropServices.DllImport("kernel32.dll")>
+    Shared Function GetEnabledXStateFeatures() As Long
+    End Function
+    Public Function SSEAVX() As Boolean
+        Try
+            If Not (GetEnabledXStateFeatures() & 4) = 0 Then
+                Return True
+            End If
+        Catch ex As Exception
+            Return False
+        End Try
+    End Function
+
+    Private Sub ButtonCheck_Click(sender As Object, e As EventArgs) Handles ButtonCheck.Click
+        If SSEAVX() Then
+            MsgBox("True")
+        Else
+            MsgBox("False")
+        End If
+    End Sub
+End Class
